87584d9a9c7a13e996e178e15d1dad82516a489d
Marc Mouawad <marc.mouawad@epfl.ch>
2026-07-04 15:30:37 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # initialize a frequency table for both
+        s_table = {}
+        t_table = {}
+        for char_t in t:
+            t_table[char_t] = 1 + t_table.get(char_t , 0)
+        for char_s in s:
+            s_table[char_s] = 1 + s_table.get(char_s , 0)
+        return t_table == s_table
