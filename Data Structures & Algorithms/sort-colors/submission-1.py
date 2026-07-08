5503775c34be7122312111e691250fb15db65a31
Marc Mouawad <marc.mouawad@epfl.ch>
2026-07-08 17:29:01 +0200

    Update: sort-colors - submission-1

--- Data Structures & Algorithms/sort-colors/submission-1.py
+++ Data Structures & Algorithms/sort-colors/submission-1.py
@@ -1,37 +1,18 @@
 class Solution:
-    def merge(self,arr,s,m,e):
-        L=arr[s:m+1]
-        R=arr[m+1:e+1]
-        i=0
-        j=0
-        k=s
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i+=1
-            else:
-                arr[k]=R[j]
-                j+=1
-            k+=1
-        while i < len(L):
-            arr[k] = L[i]
-            k+=1
-            i+=1
-        while j < len(R):
-            arr[k] = R[j]
-            k+=1
-            j+=1
-    def mergesort(self,arr,s,e):
-        if e-s+1<=1:
-            return arr
-        m = (s+e)//2
-        self.mergesort(arr,s,m)
-        self.mergesort(arr,m+1,e)
-        self.merge(arr,s,m,e)
-
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        self.mergesort(nums,0,len(nums)-1)
+        l = 0
+        r = len(nums) - 1
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i] , nums[l] = nums[l] , nums[i]
+                l+=1
+            elif nums[i] == 2:
+                nums[r] , nums[i] = nums[i] , nums[r]
+                r-=1
+                i-=1
+            i+=1
         
